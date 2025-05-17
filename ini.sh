cd0f7325b81235967030d14bf76c1a4f1b6a5060
MoonHunter233 <syhbb233@gmail.com>
2025-05-17 11:13:16 +0800

    ini.sh

---
+++ ini.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+DEV=tun0
+TUN_IP=192.168.99.1
+TUN_NET=192.168.99.0/24
+# OUTIF=wlan0  # 或者 eth0，看你实际的网络设备
+
+# 创建并配置 tun0
+ip tuntap add dev $DEV mode tun
+ip addr add $TUN_IP/24 dev $DEV
+ip link set dev $DEV up
